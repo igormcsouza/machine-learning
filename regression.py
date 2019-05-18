ea11696ca9654a4d2cdeccd1e1addd1f2b1df3ca
Igor Souza <igormcsouza@gmail.com>
2019-05-17 23:43:25 -0300

    Trying logistic regression - nto working

--- regression.py
+++ regression.py
@@ -1,6 +1,10 @@
-'''Applying Regression to solve Boston Housing Prediction Problem
-https://medium.com/@haydar_ai/learning-data-science-day-9-linear-regression-on-boston-housing-dataset-cd62a80775ef
-'''
+# Linear and Logistics regression aplication
+
+#*** Linear Regression ***
+
+#Applying Regression to solve Boston Housing Prediction Problem
+#https://medium.com/@haydar_ai/learning-data-science-day-9-linear-regression-on-boston-housing-dataset-cd62a80775ef
+
 
 from sklearn.datasets import load_boston # Import DataSet from sklearn
 from pandas import DataFrame as df
@@ -33,12 +37,43 @@ model.fit(Xtr, Ytr)
 print('MSE: ', mse(Yte, model.predict(Xte)))
 
 
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
+#import matplotlib.pyplot as plt
+#from matplotlib import rcParams
 
-Ypr = mse(Yte, model.predict(Xte))
-plt.scatter(Yte, Ypr)
-plt.xlabel("Prices")
-plt.ylabel("Predicted prices")
+#Ypr = mse(Yte, model.predict(Xte))
+#plt.scatter(Yte, Ypr)
+#plt.xlabel("Prices")
+#plt.ylabel("Predicted prices")
 # plt.plot(Xte, Ypr, color='r') # Need to figure out how does it works!
-plt.show()
+#plt.show()
+
+print("\n-------------------------------------------------------------------------\n")
+
+#*** Logistic Regression ***
+
+# What I'm trying to do now is to figure out wheter the number is prime or not
+
+def isPrime(n): 
+    # Corner case 
+    if n <= 1: 
+        return False
+    # Check from 2 to n-1 
+    for i in range(2, n): 
+        if (n % i == 0): 
+            return False
+    return True
+
+from sklearn.linear_model import LogisticRegression
+from numpy import reshape
+
+X = [i for i in range(1, 1000)]
+Y = [isPrime(i) for i in X]
+Y = [1 if i else 0 for i in Y]
+X = reshape(X,(-1,1))
+Y = reshape(Y, (-1,))
+model = LogisticRegression(solver='liblinear').fit(X, Y)
+for n in X:
+    p = reshape(n, (-1,1))
+    print("The number {0} is {1}".format(n, model.predict(p)))
+
+print('MSE: ', mse(Y, model.predict(X)))
