ef5992a1c963ab3129fa5d9bcce195a875788da9
Igor Souza <igormcsouza@gmail.com>
2019-07-10 13:54:49 -0300

    fixed validation on contact form

--- docs/js/vuejs/contact.js
+++ docs/js/vuejs/contact.js
@@ -6,6 +6,7 @@ window.onload = function(){
             errors: [],
             name: null,
             email: null,
+            reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
             phone: null,
             message: null,
             sent: false
@@ -13,25 +14,20 @@ window.onload = function(){
 
         methods: {
             submit: function (e) {
-                if (this.name && this.email && this.phone && this.message) {
-                    this.sent = true;
-                    return true;
+                if (this.name && this.email && this.phone && this.message) {                    
+                    if(this.reg.test(this.email)){
+                        this.sent = true;
+                        return true;
+                    }
                 }
           
                 this.errors = [];
           
-                if (!this.name) {
-                    this.errors.push('O nome é obrigatório.');
-                }
-                if (!this.email) {
-                    this.errors.push('O email é obrigatório.');
-                }
-                if (!this.phone) {
-                    this.errors.push('O telefone é obrigatório.');
-                }
-                if (!this.message) {
-                    this.errors.push('A mensagem é obrigatória.');
-                }
+                if (!this.name) this.errors.push('O nome é obrigatório.');
+                if (!this.email) this.errors.push('O email é obrigatório.')
+                else if(!this.reg.test(this.email)) this.errors.push('O email está incorreto!');
+                if (!this.phone) this.errors.push('O telefone é obrigatório.');
+                if (!this.message) this.errors.push('A mensagem é obrigatória.');
           
                 e.preventDefault();
               }
